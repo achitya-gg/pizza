b56a0f6432f9fcfc009af0cd4b8d0316663713bb
achitya-coder <81480404+achitya-coder@users.noreply.github.com>
2021-04-07 20:55:16 +0530

    add login and resgister

--- routes/web.js
+++ routes/web.js
@@ -1,14 +1,17 @@
 const homeController = require('../app/http/controllers/homeController')
 const cartController = require('../app/http/controllers/cartController') 
-const authContorller = require('../app/http/controllers/authControllers')
 const authController = require('../app/http/controllers/authControllers')
+const guest = require('../app/http/middlewares/guest')
 function indexRoutes(app) {
     app.get('/', homeController().index)
 
+    app.get('/login',  authController().login)
+    app.post('/login', authController().postLogin)
+    app.get('/register', guest, authController().register)
+    app.post('/register', authController().postRegister)
+    app.post('/logout', authController().logout)
+
     app.get('/cart', cartController().index)
     app.post('/update-cart', cartController().update)
-
-    app.get('/login', authController().login)
-    app.get('/register', authContorller().register)
 }
 module.exports = indexRoutes
--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const app = express()
 const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
-const PORT = process.env.PORT
+const PORT = 3000
 const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 mongoose.connect(process.env.MongoUri, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -27,11 +28,17 @@ app.use(session({
     saveUninitialized: false, 
     cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour 
 }))
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 app.use(expressLayout)
